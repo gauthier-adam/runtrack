a297366a7fa2666f2505472d58121fe5ed620e93
gauthier-adam <adam.gauthier@laplateforme.io>
2023-03-10 16:47:34 +0100

    job 2 fini

---
+++ jour5/job2/main.py
@@ -0,0 +1,7 @@
+def rectangle (width,height):
+    print ("|", "-" * (width - 2), "|")
+    for i in range (2,height):
+        print ("|", " " * (width - 2), "|")
+    print ("|", "-" * (width - 2), "|")
+
+rectangle (10,3)
